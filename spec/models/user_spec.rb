441d9bf6ac5347091b02980bf16798746bfe96a9
Brenna <brennaleker@gmail.com>
2015-07-15 07:48:49 -0700

    created a model validation spec for uniqueness of user_name and email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "does not persist non-unique user-names"
+  User.create!(user_name: 'Octothorpe', email: 'octo@email.com')
+  User.create!(user_name: 'Octothorpe', email: 'octodad@email.com')
+
+  expect(User.count).to eq 1
 end
