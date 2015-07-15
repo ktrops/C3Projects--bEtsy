6e54359b4d72f89261b22cc211bcfc3490d4dcfd
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-07-14 19:20:12 -0700

    added notes re: future validations

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,11 @@
 class Order < ActiveRecord::Base
+  # Associations ---------------------------------------------------------------
   has_many :order_items
+
+  # Validations ----------------------------------------------------------------
+  # will there be any validations?
+  # you can create an order without filling in the info
+  # so will order completion be an update method?
+
+  # Scopes ---------------------------------------------------------------------
 end
