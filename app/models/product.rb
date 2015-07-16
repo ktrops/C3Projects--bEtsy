c1899d9f4508b12babc7fc728d60108f5b16d711
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-07-16 14:07:42 -0700

    created Category & ProductCategory models to implement m-m table, updated Product Model

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,8 @@ class Product < ActiveRecord::Base
   has_many :orders, through: :order_items
   has_many :order_items
   has_many :reviews
+  has_many :product_categories
+  has_many :categories, through: :product_categories
 
   validates :name, presence: true, uniqueness: true
   validates :price, presence: true, numericality: { greater_than: 0 }
