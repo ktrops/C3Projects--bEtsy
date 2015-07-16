a7c995019a1e18fa196475744f5f6358eeb4f698
Brenna <brennaleker@gmail.com>
2015-07-16 07:38:23 -0700

    created first validation test for uniqueness

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,9 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
 
-  # it "does not persist non-unique user-names"
-  # User.create!(user_name: 'Octothorpe', email: 'octo@email.com')
-  # User.create!(user_name: 'Octothorpe', email: 'octodad@email.com')
+  it "does not persist non-unique user-names" do
+    User.create!(username: 'Octothorpe', email: 'octo@email.com', password: "himom")
+    User.create!(username: 'Octothorpe', email: 'octodad@email.com', password: "himom")
 
-  # expect(User.count).to eq 1
+    expect(User.count).to eq 1
+  end
 end
