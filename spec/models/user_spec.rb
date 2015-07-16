0cd68dfc768e758ff6cbd696d74ba0333cf650d4
Brenna <brennaleker@gmail.com>
2015-07-16 13:28:56 -0700

    adding in specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  context "Presence validations" do
+    it "Users must have a username" do
+      user = User.new(email: 'octo@email.com', password: 'himom')
+      expect(user).to_not be_valid
+    end
 
-  it "does not persist non-unique user-names" do
-    User.create!(username: 'Octothorpe', email: 'octo@email.com', password: "himom")
-    User.create!(username: 'Octothorpe', email: 'octodad@email.com', password: "himom")
+    it "Users must have an email" do
+      user = User.new(username: 'octothorpe', password: 'himom')
+      expect(user).to_not be_valid
+    end
+  end
 
-    expect(User.count).to eq 1
+ context "Uniqueness validations" do
+   let (:user) { User.new(username: 'octothorpe', email: 'octo@email.com', password: 'himom') }
+    it "Username must be unique" do
+      user2 = User.new(username: 'octothorpe', email: 'realocto@email.com', password: 'himom')
+      expect(user).to_not be_valid
+    end
+
+    it "email must be unique" do
+      user2 = User.new(username: '8thorpe', email: 'octo@email.com', password: 'himom')
+      expect(user).to_not be_valid
+    end
   end
 end
