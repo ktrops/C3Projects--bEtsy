a73f8e38bd96a3dde99a0e58d5c0d920f42913f2
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-07-16 16:08:09 -0700

    added #create and #destroy functions

--- app/controllers/product_categories_controller.rb
+++ app/controllers/product_categories_controller.rb
@@ -1,3 +1,22 @@
 class ProductCategoriesController < ApplicationController
+  def create
+    @product = Product.find(params[:product_id])
+    ProductCategory.create(product_category_params)
+    redirect_to @product
+  end
 
+  def destroy
+    @product = Product.find(params[:product_id])
+    @product_category = ProductCategory.find(params[:id])
+    @product_category.destroy
+
+    redirect_to @product
+  end
+
+  private
+
+  def product_category_params
+    { product_id: params[:product_id], 
+      category_id: params[:product_category][:category_id] }
+  end
 end
