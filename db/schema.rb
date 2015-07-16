359ddd2cc7bd45888599b9c9ce84a7fb30ad73c7
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-07-16 14:09:13 -0700

    updated seed data to reflect new models for Category implementation; updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150715135346) do
+ActiveRecord::Schema.define(version: 20150716204326) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "order_items", force: :cascade do |t|
     t.integer  "quantity"
@@ -39,11 +45,17 @@ ActiveRecord::Schema.define(version: 20150715135346) do
     t.datetime "updated_at",                        null: false
   end
 
+  create_table "product_categories", force: :cascade do |t|
+    t.integer  "product_id"
+    t.integer  "category_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.integer  "price"
     t.text     "description"
-    t.string   "category"
     t.boolean  "active"
     t.string   "photo_url"
     t.integer  "stock"
