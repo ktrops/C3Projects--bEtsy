e340b5576bc02069f62c68199510cf3f42da9d29
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-07-15 22:22:14 -0700

    uncommented @user instance variable for use in #show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,7 +12,7 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     @reviews = @product.reviews
     @order_item = OrderItem.new
-    # @user = User.find_by(id: session[:user_id])
+    @user = User.find_by(id: session[:user_id])
     # @order = Order.find(cookies[:order_id])
   end
 
