65b18f13d2e8c0caf78a659e0f4fac8efc6c71b3
Elsa <elsa.moluf@gmail.com>
2015-07-16 14:43:58 -0700

    Added Product model belongs_to User test.

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -22,14 +22,14 @@ RSpec.describe Product, type: :model do
   # -----------------
 
   describe "validates" do
-    it ":name presence" do
+    it "name is present" do
       product = Product.new
 
       expect(product).to_not be_valid
       expect(product.errors.keys).to include(:name)
     end
 
-    it ":name uniqueness" do
+    it "name is unique" do
       named_product = Product.create(name: "CIA operative identity", price: 1_000_000_000)
       same_name_product = Product.new(name: "CIA operative identity", price: 100)
 
@@ -37,14 +37,14 @@ RSpec.describe Product, type: :model do
       expect(same_name_product.errors.keys).to include(:name)
     end
 
-    it ":price presence" do
+    it "price is present" do
       product = Product.new
 
       expect(product).to_not be_valid
       expect(product.errors.keys).to include(:price)
     end
 
-    it ":price numericality" do
+    it "price is a number" do
       lol_product = Product.new(name: "lol", price: "lol")
 
       expect(lol_product).to_not be_valid
@@ -56,23 +56,46 @@ RSpec.describe Product, type: :model do
       expect(yay_product).to be_valid
     end
 
-    it ":price > 0" do
-      negative_product = Product.new(name: "negative", price: -1)
-
-      expect(negative_product).to_not be_valid
-      expect(negative_product.errors.keys).to include(:price)
 
 
-      zero_product = Product.new(name: "zero", price: 0)
+    context "price > 0" do
+      it "price can't be negative" do
+        negative_product = Product.new(name: "negative", price: -1)
 
-      expect(zero_product).to_not be_valid
-      expect(zero_product.errors.keys).to include(:price)
+        expect(negative_product).to_not be_valid
+        expect(negative_product.errors.keys).to include(:price)
+      end
+
+      it "price can't be 0" do
+        zero_product = Product.new(name: "zero", price: 0)
+
+        expect(zero_product).to_not be_valid
+        expect(zero_product.errors.keys).to include(:price)
+      end
 
 
-      positive_product = Product.new(name: "positive", price: 1)
+      it "price can be more than 1" do
+        positive_product = Product.new(name: "positive", price: 1)
 
-      expect(positive_product).to be_valid
+        expect(positive_product).to be_valid
+      end
     end
+
+    context "associations" do
+      it "belongs to User" do
+        hacker = User.create(id: 70, username: "Hackman", email: "hackman@hackers.hack", password: "password", password_confirmation: "password")
+        nk_product = Product.create(name: "Kim Jeong Un's twitter", price: 100_000, user_id: 70)
+
+        expect(nk_product.user.username).to eq("Hackman")
+      end
+    end
+
+    # belongs_to user
+    # has_many orders through order_items
+    # has_many order_items
+    # has_many reviews
+
+    # toggle_active!
 
   end
 
