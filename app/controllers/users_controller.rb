15e885502c794c51834675e4aadbaa49c85e83ef
Brenna <brennaleker@gmail.com>
2015-07-16 17:54:00 -0700

    User#New now flashing success message

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,15 +3,26 @@ class UsersController < ApplicationController
   before_action :require_login, except: [:new, :create]
 
   def new
-
+    @user = User.new(user_params[:user])
   end
 
+# trip login so user is logged in as soon as they create the account
   def create
-
+    @user = User.create(user_params[:user])
+    if @user.save
+      redirect_to login_path
+      flash[:success] = "Registration successful, take your shiny new username and password for a spin"
+    else
+      flash.now[:errors] = "Registration invalid, please try again."
+      render new_user_path
+    end
   end
 
   def show
+    @user_id = params[:id]
+    @user = User.find(@user_id)
 
+    render :show
   end
 
 
@@ -23,4 +34,7 @@ class UsersController < ApplicationController
     redirect_to login_path unless session[:user_id]
   end
 
+  def user_params
+    params.permit(user: [:username, :email, :password, :password_confirmation])
+  end
 end
