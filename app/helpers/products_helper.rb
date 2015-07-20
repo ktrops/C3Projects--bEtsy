092397f4751c12bb47ed46c8cd6b8c19a6621f51
Elsa <elsa.moluf@gmail.com>
2015-07-20 00:08:23 -0700

    Added styling mainly to product index and show views.

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -2,4 +2,19 @@ module ProductsHelper
   def active_text(product)
     product.active ? "active" : "inactive"
   end
+
+  def rating_to_stars(rating)
+    case rating
+    when 1
+      "★☆☆☆☆"
+    when 2
+      "★★☆☆☆"
+    when 3
+      "★★★☆☆"
+    when 4
+      "★★★★☆"
+    when 5
+      "★★★★★"
+    end
+  end
 end
