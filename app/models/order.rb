455e13d30f0bdb079016c01cdbda15e89e228951
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-07-20 22:12:23 -0700

    added association products, through: :order_items

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,7 @@
 class Order < ActiveRecord::Base
   # Associations ---------------------------------------------------------------
   has_many :order_items
+  has_many :products, through: :order_items
 
   # Validations ----------------------------------------------------------------
   validates :status, inclusion: { in: %w(pending paid complete cancelled), 
