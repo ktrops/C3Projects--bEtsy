4d8bba891e9af4430c5c26832bd328c33f0ce83e
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-07-20 23:17:44 -0700

    added validations for checkout form

--- app/models/order.rb
+++ app/models/order.rb
@@ -7,7 +7,12 @@ class Order < ActiveRecord::Base
   validates :status, inclusion: { in: %w(pending paid complete cancelled), 
     message: "That is not a valid order status" }
   validates :cc_number, numericality: { only_integer: true }, 
-    length: { in: 15..16 }, allow_nil: true
+    length: { in: 15..16 }, on: :update
+  validates :address1, :city, :state, presence: true, on: :update
+
+  validates :cc_cvv, numericality: { only_integer: true }, length: { is: 3 }, on: :update
+  validates :billing_zip, numericality: { only_integer: true }, length: { is: 5 }, on: :update
+  validates :mailing_zip, numericality: { only_integer: true }, length: { is: 5 }, on: :update
 
   # Scopes ---------------------------------------------------------------------
   def total
