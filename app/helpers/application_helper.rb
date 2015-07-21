8f3dd32e025e884302c622b71542b763b1431b51
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-07-20 20:19:37 -0700

    created #readable_date helper method

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def readable_price(price)
     number_to_currency(price/100.0)
   end
+
+  def readable_date(datetime)
+    datetime.strftime("%b %d, %Y, at %I:%M%P")
+  end
 end
