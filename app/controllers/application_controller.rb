7d0bb5468e5fe944eaef06bdc71ffe468bbfd237
Brenna <brennaleker@gmail.com>
2015-07-21 17:28:35 -0700

    WIP - fixing authenticated page bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,4 +18,28 @@ class ApplicationController < ActionController::Base
       Order.new
     end
   end
+
+  # def require_login
+  #   redirect_to login_path unless session[:user_id]
+  # end
+
+  def authenticate_user
+    if session[:user_id]
+       # set current user object to @current_user object variable
+      @current_user = User.find session[:user_id]
+      return true
+    else
+      redirect_to(login_path)
+      return false
+    end
+  end
+
+  def save_login_state
+    if session[:user_id]
+      redirect_to user_path(@user.id)
+      return false
+    else
+      return true
+    end
+  end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  before_action :authenticate_user, except: [:merchant, :index]
 
   def index
     @products = Product.all
