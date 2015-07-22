767edb986680c4c7727a5ddadb3937b23eb2c960
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-07-22 10:47:25 -0700

    fixed cart bug

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -6,21 +6,17 @@ class OrderItemsController < ApplicationController
     previous_product = order.products.find_by(id: product.id)
 
     if previous_product
-      # previous_order_item = OrderItem.where(order_id: order.id).find_by(product_id: product.id)
-      previous_order_item = order.order_items.where(product_id: previous_product.id)
+      previous_order_item = order.order_items.find_by(product_id: previous_product.id)
       previous_order_item.quantity += (params[:order_item][:quantity]).to_i
       previous_order_item.save
       flash[:success] = "You have added #{params[:order_item][:quantity]} x #{product.name} to your cart."
     else # new product
-      # order_item = order.order_items.create(quantity: params[:order_item][:quantity], 
-      #   product_id: product.id, item_total: (product.price * (params[:order_item][:quantity]).to_i))
       order_item = order.order_items.new(order_item_params)
       order_item.product = product
       order_item.item_total = product.price * order_item.quantity
       order_item.save
       if order_item.save
         flash[:success] = "You have added #{order_item.quantity} x #{product.name} to your cart."
-        # session[:order_id] = order.id
       else
         flash[:errors] = "There was a problem with adding this item to your cart."
         if order.order_items.count == 0
@@ -28,25 +24,27 @@ class OrderItemsController < ApplicationController
           session[:order_id] = nil
         end
       end
-        redirect_to product
     end
+    
+    redirect_to product
   end
 
   def update
     order_item = OrderItem.find(params[:id])
     old_quantity = order_item.quantity
     order_item.update(order_item_params)
+    order_item.item_total = order_item.quantity * order_item.product.price
+    order_item.save
     if order_item.save
       if order_item.quantity == old_quantity
         flash[:success] = "No change was made."
       else
         flash[:success] = "Your order has been updated."
       end
-      redirect_to cart_path
     else
       flash[:errors] = "There was a problem with your update."
-      redirect_to cart_path
     end
+      redirect_to cart_path
   end
 
   def destroy
@@ -61,13 +59,11 @@ class OrderItemsController < ApplicationController
 
   def cart
     @order = Order.find_by(id: session[:order_id])
-    # raise
-    # @order_items = OrderItem.where(order_id: session[:order_id])
   end
 
   private
 
   def order_item_params
-    params.require(:order_item).permit(:quantity)
+    params.require(:order_item).permit(:quantity, :item_total)
   end
 end
