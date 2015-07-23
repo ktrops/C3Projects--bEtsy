250860020e73f2a378965591c7683efed611ef7c
Elsa <elsa.moluf@gmail.com>
2015-07-23 10:04:29 -0700

    WIP converting order fulfillment page into styled tables.

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -73,33 +73,6 @@ class OrdersController < ApplicationController
 
     @filtered_order_items = OrderItem.joins(:order, :product).where(orders: { status: params[:status] }, products: { user_id: @user.id })
 
-    # # collect the Order IDs based on the user's particular OrderItems
-    # # disparate associations == ow
-    # order_ids = []
-    # @order_items.each do |order_item|
-    #   order_ids << order_item.order_id
-    # end
-    #
-    # # the user's orders
-    # users_orders = Order.where(:id => order_ids )
-    # # the user's orders filtered by a status
-    # orders_by_a_status = users_orders.where(status: params[:order])
-    #
-    # order_status_ids = []
-    # orders_by_a_status.each do |order|
-    #   order_status_ids << order.id
-    # end
-    #
-    # # collect the Order IDs of only that status
-    # filtered_order_ids = []
-    # @order_items.each do |order_item|
-    #   if order_status_ids.include?(order_item.order_id)
-    #     filtered_order_ids << order_item.order_id
-    #   end
-    # end
-    #
-    # @filtered_order_items = OrderItem.where(:order_id => filtered_order_ids)
-    # @total_revenue = @filtered_order_items.sum(:item_total)
     render :fulfillment
   end
 
