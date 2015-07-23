ae8dcde2d409bd8f713dc10de748be2d7fc64b97
Katie Atrops <katrops@gmail.com>
2015-07-22 20:41:48 -0700

    changed home page product list limit to 10

--- app/models/product.rb
+++ app/models/product.rb
@@ -22,7 +22,7 @@ class Product < ActiveRecord::Base
 
   # implement by ranking?
   def self.front_page_list
-    self.active_products(self.all.limit(20))
+    self.active_products(self.all.limit(10))
   end
 
 
