66389465d58e7947a4978497406e98cf68eb6b36
Elsa <elsa.moluf@gmail.com>
2015-07-23 17:46:08 -0700

    WIP starting reviews controller tests.

--- spec/controllers/reviews_controller_spec.rb
+++ spec/controllers/reviews_controller_spec.rb
@@ -5,15 +5,38 @@ RSpec.describe ReviewsController, type: :controller do
   describe "GET #new" do
     it "returns http success" do
       get :new
-      expect(response).to have_http_status(:success)
+
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
     end
   end
 
   describe "GET #create" do
     it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
+      post :create, product_id: product.id, review_params
+      let(:product) do
+        {
+          product: {
+            id: 1,
+            name: "lol",
+            price: 100,
+          }
+        }
+      end
+      let(:review_params)do
+        {
+          review: {
+            rating: 5,
+            review_text: "lol"
+          }
+        }
+      end
+
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
     end
   end
+
+
 
 end
