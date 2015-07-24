9abce65455507bf3dbcdf773f8aff1233b13505c
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-07-23 20:25:45 -0700

    fixed specs

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -62,17 +62,4 @@ RSpec.describe Order, type: :model do
       end
     end
   end
-
-  # context "associations" do
-  #   let(:order) { Order.create() }
-  #   let(:user) { User.create(username: "Ms. Spy") }
-  #   let(:product) { Product.create(name: "secrets", user_id: user.id) }
-  #   it "can access order_items" do
-  #     3.times do
-  #       OrderItem.create(product_id: product.id, order_id: order.id)
-  #     end
-
-  #     expect(order.order_items.count).to eq 3
-  #   end
-  # end
 end
