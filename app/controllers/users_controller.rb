fd0bd90b1a7020d9854775f9e3032179b82efc86
Brenna <brennaleker@gmail.com>
2015-07-24 12:11:15 -0700

    switched params method in users controller back to original

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+    params.permit(user: [:username, :email, :password, :password_confirmation])
   end
 
   def total_sales
