409600a857a44c616ee6eab32de0c8b2c2531dda
Alice Rhomieux <alice.rhomieux@gmail.com>
2015-08-13 17:01:19 -0700

    added comment to #readable_date to show example of the output format

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,7 @@ module ApplicationHelper
     number_to_currency(price/100.0)
   end
 
+  # for example: Jul 24, 2015, at 06:50pm
   def readable_date(datetime)
     datetime.strftime("%b %d, %Y, at %I:%M%P")
   end
